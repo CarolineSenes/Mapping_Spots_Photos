5a3371bce048980f987192f9f7631c0e211ebbaa
SENES <caro67200@gmail.com>
2022-01-17 09:05:57 +0100

    popup place

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
-import { MyLocation } from '@mui/icons-material';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { MyLocation, Star } from "@mui/icons-material";
+import "./app.css";
 
 const REACT_APP_MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiY2Fyb2xpbmVzZW5lcyIsImEiOiJja3lnNTNlbXIxcGEwMnZwYnd1dmNwZG9vIn0.qstETxxcAmij4x9d9bN1ew";
@@ -28,8 +29,35 @@ function App() {
           offsetLeft={-20}
           offsetTop={-10}
         >
-          <MyLocation style={{ color:"red" }}/>
+          <MyLocation style={{ color: "red" }} />
         </Marker>
+        <Popup
+          latitude={48.5734053}
+          longitude={7.7521113}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left"
+        >
+          <div className="card">
+            <label>Lieu</label>
+            <h4 className="place">Cathédrale de Strasbourg</h4>
+            <label>Review</label>
+            <p className="desc">Lieu magnifique!</p>
+            <label>Note</label>
+            <div className="stars">
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+              <Star className="star" />
+            </div>
+            <label>Information</label>
+            <span className="username">
+              Créé par <b>Caro</b>
+            </span>
+            <span className="date">il y a 1h</span>
+          </div>
+        </Popup>
       </ReactMapGL>
     </div>
   );
